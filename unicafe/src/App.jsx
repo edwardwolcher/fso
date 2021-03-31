7072477c695a20b560ffcd23179e7491d56944c7
Edward Wolcher <65256014+edwardwolcher@users.noreply.github.com>
2021-03-31 16:04:27 -0700

    1.6

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
 
+const Button = ({ name, handleClick }) => (
+  <button onClick={handleClick}>{name}</button>
+);
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const incriment = (value, setter) => {
+    setter(value + 1);
+  };
+
+  return (
+    <div>
+      <h1>Unicafe</h1>
+      <h2>Give Feedback</h2>
+      <div>
+        <Button name="good" handleClick={() => incriment(good, setGood)} />
+        <Button
+          name="neutral"
+          handleClick={() => incriment(neutral, setNeutral)}
+        />
+        <Button name="bad" handleClick={() => incriment(bad, setBad)} />
+      </div>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
 };
 
 export default App;
