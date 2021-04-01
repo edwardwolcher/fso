fe20fee782ed7207ee6011564b15694bcb650aea
Edward Wolcher <65256014+edwardwolcher@users.noreply.github.com>
2021-03-31 17:57:23 -0700

    2.1

--- course-information-app/src/App.jsx
+++ course-information-app/src/App.jsx
@@ -10,43 +10,53 @@ const Part = ({ part }) => (
 
 const Content = ({ parts }) => (
   <div>
-    {parts.map((part, index) => (
-      <Part key={index} part={part} />
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
     ))}
   </div>
 );
 
 const Total = ({ parts }) => (
   <p>
-    Number of exercises{" "}
+    Number of exercises:{" "}
     {parts.reduce((total, part) => (total += part.exercises), 0)}
   </p>
 );
 
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   );
 };
