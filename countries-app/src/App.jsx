6cc404782b2c367460023812e2893d30a501d23c
Edward Wolcher <65256014+edwardwolcher@users.noreply.github.com>
2021-04-01 17:50:07 -0700

    logic working

--- countries-app/src/App.jsx
+++ countries-app/src/App.jsx
@@ -1,9 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () => {
+// data endpoint
+const dataUrl = "https://restcountries.eu/rest/v2/all";
+
+// components
+
+const Input = ({ value, setter, type = "text", id = null }) => {
+  const handleChange = (e) => {
+    setter(e.target.value);
+  };
+  return <input type={type} id={id} value={value} onChange={handleChange} />;
+};
+
+const SearchField = ({ searchTerm, setSearchTerm }) => {
+  const searchFieldLabel = "Find Countries: ";
+  const searchFieldId = "input-searchfield";
   return (
     <div>
-      <h1>App</h1>
+      <label htmlFor={searchFieldId}>{searchFieldLabel}</label>
+      <Input value={searchTerm} setter={setSearchTerm} id={searchFieldId} />
+    </div>
+  );
+};
+
+const CountryListItem = ({ country }) => <li>{country.name}</li>;
+
+const CountryList = ({ countries }) => {
+  return (
+    <ul>
+      {countries.map((country) => (
+        <CountryListItem key={country.alpha3Code} country={country} />
+      ))}
+    </ul>
+  );
+};
+
+const CountryDisplay = ({ country }) => {
+  return (
+    <div>
+      <h2>{country.name}</h2>
+    </div>
+  );
+};
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(dataUrl);
+        setCountries(response.data);
+      } catch (error) {
+        // TODO -- error handling
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const countriesToDisplay = countries.filter((country) =>
+    country.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  let countryDisplay;
+
+  if (searchTerm.length === 0) {
+    countryDisplay = <p>Enter search term above</p>;
+  } else if (countriesToDisplay.length <= 0) {
+    countryDisplay = <p>No countries match query</p>;
+  } else if (countriesToDisplay.length === 1) {
+    countryDisplay = <CountryDisplay country={countriesToDisplay[0]} />;
+  } else if (countriesToDisplay.length <= 10) {
+    countryDisplay = <CountryList countries={countriesToDisplay} />;
+  } else {
+    countryDisplay = <p>Too many results</p>;
+  }
+
+  return (
+    <div>
+      <h1>Countries</h1>
+      <SearchField searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      {countryDisplay}
     </div>
   );
 };
