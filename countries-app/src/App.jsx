1bd2f5f2ccda39726cbaf401ad31fcb81c142d63
Edward Wolcher <65256014+edwardwolcher@users.noreply.github.com>
2021-04-01 17:56:47 -0700

    2.11

--- countries-app/src/App.jsx
+++ countries-app/src/App.jsx
@@ -40,6 +40,15 @@ const CountryDisplay = ({ country }) => {
   return (
     <div>
       <h2>{country.name}</h2>
+      <p>Capital: {country.capital}</p>
+      <p>Population: {country.population}</p>
+      <h3>Languages</h3>
+      <ul>
+        {country.languages.map((language) => (
+          <li key={language.iso639_2}>{language.name}</li>
+        ))}
+      </ul>
+      <img width="150px" src={country.flag} alt="National Flag" />
     </div>
   );
 };
